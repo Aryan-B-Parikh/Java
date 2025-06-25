11a812d8d095457add6b4a17311462485d5ed764
Aryan-B-Parikh <aryan81006@gmail.com>
2025-06-25 20:10:27 +0530

    added new code

---
+++ Basics/AddUsingKeyboard.java
@@ -0,0 +1,14 @@
+package Basics;
+import java.util.Scanner;
+class Add{
+    public static void main(String[] a){
+        Scanner s = new Scanner(System.in);
+        int x,y,sum;
+        System.out.println("Enter first Number:");
+        x=s.nextInt();
+        System.out.println("Enter second Number:");
+        y=s.nextInt();
+        sum = x + y;
+        System.out.println("Sum is :" + sum);
+    }
+}
