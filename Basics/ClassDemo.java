ef6aa4bea1c0a0c22c91c44897fb616fb0e5e47c
Aryan-B-Parikh <aryan81006@gmail.com>
2025-06-25 15:16:44 +0530

    Added new code

---
+++ Basics/ClassDemo.java
@@ -0,0 +1,18 @@
+package Basics;
+class Test {
+    int a;
+    int b;
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+}
+
+public class ClassDemo {
+    public static void main(String[] args) {
+        Test t = new Test();
+        t.a = 9;
+        t.b = 6;
+        System.out.println(t.sum(t.a, t.b));
+    }
+}
