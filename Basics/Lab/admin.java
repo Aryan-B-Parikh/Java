7fe9d85a3a51e04b200d211ab4ade6d979682847
Aryan-B-Parikh <aryan81006@gmail.com>
2025-07-02 16:01:15 +0530

    Added new codes

--- Basics/Lab/admin.java
+++ Basics/Lab/admin.java
@@ -1,4 +1,27 @@
-public class admin{
+package Basics.Lab;
+
+import java.util.Scanner;
+
+public class Admin {
+
     public static void main(String[] args) {
-        System.out.println("I am Admin !");
+        Scanner sc = new Scanner(System.in);
+        Student s = new Student();
+        if (s instanceof Student) {
+            System.out.print("Enter student ID: ");
+            int id = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Enter student name: ");
+            String name = sc.nextLine();
+
+            s.setStdId(id);
+            s.setStdName(name);
+
+            System.out.println("Student ID: " + s.getStdId());
+            System.out.println("Student Name: " + s.getStdName());
+        } else {
+            System.out.println("Object is not an instance of Student class.");
+        }
+    }
 }
