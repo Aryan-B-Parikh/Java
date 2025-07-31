fa78299fdaa795af673415f663a3d8406456cac7
Aryan-B-Parikh <aryan81006@gmail.com>
2025-07-31 20:40:06 +0530

    Minor change

--- Lab/Lab_Work/230725/Task2307.java
+++ Lab/Lab_Work/230725/Task2307.java
@@ -3,10 +3,8 @@ class Task2307{
     public static void main(String[] args) {
         Person p=new Student("Aryan", 18, 3);
         Person t=new Teacher("Dr. OM", 38, "Mathematics");
-
         t.age = p.age;
         t.name = p.name;
-
         System.out.println("Teacher Details:");
         t.getDetails();
     }
