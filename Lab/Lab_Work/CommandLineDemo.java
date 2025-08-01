830fa49d09b00936e426669cb8e3625f676e0653
Aryan-B-Parikh <aryan81006@gmail.com>
2025-08-01 21:09:47 +0530

    Minor Changes

--- Lab/Lab_Work/CommandLineDemo.java
+++ Lab/Lab_Work/CommandLineDemo.java
@@ -1,11 +1,14 @@
 package Basics.Lab.Lab_Work;
-
-class CommandLineDemo{
-    public static void main(String[] args){
-       if(args.length>0){
+class CommandLineDemo
+{
+    public static void main(String[] args)
+    {
+       if(args.length>0)
+       {
             System.out.println("Hello " + args[0] + "!");
         } 
-        else {
+        else 
+        {
             System.out.println("Hello, World!");
         }
     }
--- Lab/Lab_Work/ReadingFromKeyboard.java
+++ Lab/Lab_Work/ReadingFromKeyboard.java
@@ -1,7 +1,9 @@
 package Basics.Lab.Lab_Work;
 import java.util.Scanner;
-class Main{
-    public static void main(String[] a){
+class Main
+{
+    public static void main(String[] a)
+    {
         Scanner s = new Scanner(System.in);
         String name;
         System.out.println("Enter your name:");
