28cfa1a9beab12ae50b78ccb1ff9d32476b16eb8
Aryan-B-Parikh <aryan81006@gmail.com>
2025-09-02 23:30:24 +0530

    Codes

---
+++ Self/TicketMain.java
@@ -0,0 +1,17 @@
+class TicketSystem{
+int count;
+TicketSystem(int count){
+    this.count=count;
+}
+public void bookTicket(int count) { 
+ 
+}
+public void cancelTicket(int count) { 
+   
+}
+}
+public class TicketMain {
+    
+} TicketMain {
+    
+}
